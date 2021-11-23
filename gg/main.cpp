4f88b6f1834cfd5dd5e31aa8427b5d0f1f57efcc
pavukkkkk <kirilenkokonstantin2033@gmail.com>
2021-11-23 17:32:25 +0400

    Initial Commit

---
+++ gg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gg
+//
+//  Created by Константин Кириленко on 23.11.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
